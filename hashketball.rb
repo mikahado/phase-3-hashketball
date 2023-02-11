008bd193ab1d8869c7ab43a92467cb90503309b3
Michael <mikahado@gmail.com>
2023-02-11 01:28:00 -0500

    completes lab

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,6 @@
 # Write your code below game_hash
+require "pry"
+
 def game_hash
   {
     home: {
@@ -125,5 +127,73 @@ def game_hash
     }
   }
 end
+
+# helper method to get all players
+
+
+def num_points_scored(player_name)
+  player = player_stats(player_name)
+  player[:points]
+end
+
+
+
+def shoe_size(player_name)
+  player = player_stats(player_name)
+  player[:shoe]
+end
+
+
+
+def team_colors(team_name)
+  team = find_team(team_name)
+  team[:colors]
+end
+
+def team_names
+  game_hash.map do |location, team_data|
+    team_data[:team_name]
+  end
+end
+
+def player_numbers(team_name)
+  team = find_team(team_name)
+  team[:players].map do |n|
+    n[:number]
+  end
+end
+
+
+
+def big_shoe_rebounds
+  big_shoe_player[:rebounds]
+end
+
+private
+
+def all_players
+  game_hash[:home][:players] +
+  game_hash[:away][:players]
+end
+
+def player_stats(player_name)
+  all_players.find do |player|
+    player[:player_name] == player_name
+  end
+end
+
+def find_team(team_name)
+  team_info = game_hash.find do |location, team_data|
+    team_data[:team_name] == team_name
+  end
+  team_info[1]
+end
+
+def big_shoe_player
+  all_players.max_by do |player|
+    player[:shoe]
+  end
+end
+
 
 # Write code here
